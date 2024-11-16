327848d5af541f04c03bc714a0d6a8c36a7c05cb
Franck Li <fli@made-f0Br6s6.clusters.42paris.fr>
2024-11-16 17:14:35 +0100

    start getters

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -2,12 +2,48 @@
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
 
+# include <string>
+# include <signal.h>
+# include <vector>
+
+# include "Client.hpp"
+
 class Channel {
 	private:
 		int _id;
-
+		int	_maxUsers;
+		bool	_userLimited;
+		bool	_restricted;
+		bool	_inviteOnly;
+		bool	_topicRestrict;
+		std::string		_password;
+		std::string		_name;
+		std::string		_topic;
+		std::vector<Client	&>	_op;
+		std::vector<const int>	_clientsIDs;
 	public:
+		Channel();
+		~Channel();
+
+		//Accessors
 		int const& getID(void);
+		int const& getMaxUser(void);
+		bool const& isUserLimited(void);
+		bool const& isRestricted(void);
+		bool const& isInviteOnly(void);
+		bool const& isTopicRestricted(void);
+		std::string const& getPassword(void);
+		std::string const& getName(void);
+		std::string const& getTopic(void);
+		std::vector<Client	&>	getOperators(void);
+		std::vector<const int>	getClientIDs(void);
+
+
+		//COMMAND
+		void cmd_kick(Client	&executor, Client	&target);
+		void cmd_invite(Client	&executor, Client	&target);
+		void cmd_topic(Client	&executor, std::string topic);
+		void cmd_mode(Client	&executor, std::string mode);
 };
 
 #endif
--- srcs/classes/Channel.cpp
+++ srcs/classes/Channel.cpp
@@ -1,5 +1,32 @@
 #include "Channel.hpp"
 
+Channel::Channel()
+{
+}
+
+Channel::~Channel()
+{
+}
+
 int const& Channel::getID(void) {
 	return this->_id;
+};
+
+int const& Channel::getMaxUser(void)
+{
+	return this->_maxUsers;
+}
+
+bool const& Channel::isUserLimited(void)
+{
+	return this->_userLimited;
+}
+
+bool const& Channel::isRestricted(void)
+{
+	return this->_restricted;
+}
+
+std::string const& Channel::getName(void) {
+	return this->_name;
 };
